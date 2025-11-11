f124dca0adb069b1ec33d14cdf0a1823ff62397e
12ed3e34dw <pvlad8056@gmail.com>
2025-11-11 21:23:05 +0200

    init

--- src/app/Practice/page.tsx
+++ src/app/Practice/page.tsx
@@ -1,10 +1,17 @@
 'use client';
 import "@/Styles/Practice_styles/Practice_styles.css";
-
+import Menu_Contacts from "@/navigate/Menu_Contacts";
+import Menu from "@/navigate/Menu";
+import Image from "next/image";
+import Birds from "@/assets/1024+/Birds.png";
+import Food from "@/assets/1024+/Food.png";
+import Weather from "@/assets/1024+/Weather.png";
+import Wild_animals from "@/assets/1024+/Wild_animals.png";
 export default function Page() {
 
     return (
         <>
+            <Menu/>
             <div className="grid-section-button">
         <div className="button_training">
             <div className="txt_button_training">
@@ -66,13 +73,13 @@ export default function Page() {
 
             <div className="grid-section-button-lvl">
                 <div className="button_lvl_a1_a2">
-                    <div className="">A1-A2</div>
+                    <div className="button_lvl_a1_a2_txt">A1-A2</div>
                 </div>
                 <div className="button_lvl_b1_b2">
-                    <div className="">B1-B2</div>
+                    <div className="button_lvl_b1_b2_lvl">B1-B2</div>
                 </div>
-                <div className="button_lvl_c1_c2_">
-                    <div className="">C1-C2</div>
+                <div className="button_lvl_c1_c2">
+                    <div className="button_lvl_c1_c2_txt">C1-C2</div>
                 </div>
             </div>
 
@@ -82,23 +89,122 @@ export default function Page() {
 
 
             <div className="grid-section-cont-practice">
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
+                <div className="cont_practice_1">
+                <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_2">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_3">
+                    <div className="cont_practice_1_txt">10 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Birds} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Birds</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_4">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_5">
+                    <div className="cont_practice_1_txt">18 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Weather} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Weather</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_6">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
             </div>
 
             <div className="grid-section-cont-practice-2">
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
+                <div className="cont_practice_7">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_8">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_9">
+                    <div className="cont_practice_1_txt">55 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Food} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Food</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_10">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_11">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
+                <div className="cont_practice_12">
+                    <div className="cont_practice_1_txt">100 words</div>
+                    <div className="cont_practice_1_txt_2">C1-C2</div>
+                    <Image src={Wild_animals} alt="Adults" className="Ico"/>
+                    <div className="cont_practice_1_txt_3">Wild animals</div>
+                    <div className="cont_practice_1_button">
+                        <div className="cont_practice_1_button_txt">Practice</div>
+                    </div>
+                </div>
             </div>
-
+            <div className="button_More_curses">
+                <div className="button_More_curses_txt">More</div>
+            </div>
+            <Menu_Contacts/>
         </>
     )
 }
